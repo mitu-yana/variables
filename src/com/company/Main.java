7c1ad84e052d62bb421c181faa07115d7e863c9e
mitu-yana <mitu-yana@mail.ru>
2021-12-03 01:42:50 +0300

    I created programme with variables

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double a;
+        a = 25.785;
+        byte b = 127;
+    }
+}
