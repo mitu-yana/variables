9605d1b4376539724b01f2b511ad16d008d5396a
mitu-yana <mitu-yana@mail.ru>
2021-12-05 02:19:05 +0300

    Revert "I created programme with massive"
    This reverts commit 1139cc3721b7a3addcace64da64aa9f93bd3caf7.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,12 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        int[] array = {11, 17, 28, 4, 9};
-        int x = array[0];
-        array[0] = array[array.length-1];
-        array[array.length-1] = x;
-        for (int i = 0; i < 5; i++){
-                System.out.print(" " + array[i] + " ");
-            }
-        System.out.println(" ");
-        int y = array[0]+array[(array.length-1)/2];
-               System.out.println(y);
+	double a;
+        a = 25.785;
+        byte b = 127;
     }
 }
